64564a0b2593b3424416a5788212759fb7e5bece
Cesar Santana <casc.santana@gmail.com>
2018-02-24 14:38:10 -0600

    Creating method to get list of users

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,19 +1,19 @@
 import Service from 'easy-requests'
-import { API_URL } from 'src/config/api'
-import Config from 'src/config' // It will changed to config OAuth
+import { Fire, Config } from 'helpers'
 
 class User extends Service {
   constructor () {
     super()
-    this.config.origin = API_URL
+    // this.config.origin = Env('BASE_URL')
+    this.config.origin = Config('API_URL')
     this.config.endpoint = '/users/'
   }
 
   static async currentUser () {
     let UserService = new User()
-    console.log(Config('CURRENT_USER_URL'))
     try {
       let response = await UserService.http.get(Config('CURRENT_USER_URL'))
+      Fire('user:fetched', response)
       return response
     } catch (error) {
       throw error
--- src/store/users/mutations.js
+++ src/store/users/mutations.js
@@ -1,3 +1,7 @@
 export const setCurrentUser = (state, payload) => {
   state.currentUser = payload.data
 }
+
+export const setUsersList = (state, payload) => {
+  state.users = payload
+}
