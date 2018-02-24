9ebbc9628886afe9d47ce99b083b3edc10936c73
Cesar Santana <casc.santana@gmail.com>
2018-02-24 17:42:06 -0600

    Refactoring code in config files and improve Config helper

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,14 +5,14 @@ class User extends Service {
   constructor () {
     super()
     // this.config.origin = Env('BASE_URL')
-    this.config.origin = Config('API_URL')
+    this.config.origin = Config('api.api_url')
     this.config.endpoint = '/users/'
   }
 
   static async currentUser () {
     let UserService = new User()
     try {
-      let response = await UserService.http.get(Config('CURRENT_USER_URL'))
+      let response = await UserService.http.get(Config('api.current_user_url'))
       Fire('user:fetched', response)
       return response
     } catch (error) {
