8159b572b32d8a8df44f45b8531e51871f8caf43
Paullaguno <paulina.llagunogr@uanl.edu.mx>
2018-03-21 14:00:40 -0600

    created d-ingresar view with styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   <div>
-      <section id="d_start">
+      <!-- <section id="d_start">
         <div class="container">
           <div class="row">
             <div class="col-sm-12">
@@ -14,6 +14,30 @@
             </div>
             <div class="col-sm-12">
               <a href="#">Ya estoy registrado</a>
+            </div>
+          </div>
+        </div>
+      </section> -->
+
+      <section id="d_ingresos">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <img src="" alt="">
+            </div>
+            <div class="col-sm-12">
+              <h1>Ingresos</h1>
+              <p>Especifica tus ingresos mensuales</p>
+            </div>
+            <div class="col-sm-12">
+              <form action="">
+                <div class="input-field add-ingresos">
+                  <input type="text" name="ingresos" id="ingresos" placeholder="$10,000.00">
+                </div>
+                <div class="input-field">
+                  <button type="submit" class="btn" id="siguiente">Siguiente</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
@@ -38,6 +62,20 @@
   $purple:  #3f224c;
   $white:   #FFF;
   $blue:    #64c9db;
+  $green:   #c0d84a;
+  $gray:    #dedede;
+
+  .btn {
+    background-color: $blue;
+    color: $white;
+    font-size: 20px;
+    font-weight: bold;
+    padding: 14px 60px;
+    text-decoration: none;
+    border-radius: 15px;
+    border: none;
+    cursor: pointer;
+  }
 
   #d_start {
     position: relative;
@@ -60,22 +98,64 @@
 
         .start-now {
           margin-bottom: 106px;
-
-           .btn {
-            background-color: $blue;
-            color: $white;
-            font-size: 20px;
-            font-weight: bold;
-            padding: 14px 30px;
-            text-decoration: none;
-            border-radius: 15px;
-          }
         }
 
         a {
           color: $purple;
           font-size: 18px;
           font-weight: bold;
+        }
+      }
+    }
+  }
+
+  #d_ingresos {
+    position: relative;
+    min-height: 100vh;
+    height: auto;
+    width: 100%;
+    text-align: center;
+    color: $purple;
+    padding-bottom: 150px;
+
+    .container {
+      .row {
+        h1 {
+          font-size: 35px;
+          font-weight: bold;
+          margin-top: 57px;
+          margin-bottom: 10px;
+        }
+
+        p {
+          font-size: 20px;
+          line-height: 25px;
+          margin-bottom: 150px;
+        }
+
+        form {
+          .input-field {
+            position: relative;
+            width: 30%;
+            left: 35%;
+
+            input {
+              border: none;
+              border-bottom: 1px solid $gray !important;
+              font-size: 60px;
+              color: $green;
+              text-align: center;
+              width: 100%;
+            }
+
+            input:focus {
+              outline: none;
+            }
+          }
+
+          .add-ingresos {
+            margin-bottom: 150px;
+          }
         }
       }
     }
