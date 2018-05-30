547f9c2fd72708c6ccc7a8d31f5a55fc3ea00145
ema18787 <emanueljtc@gmail.com>
2018-05-30 11:10:40 -0400

    margin top meta

--- src/pages/ahorro.vue
+++ src/pages/ahorro.vue
@@ -264,6 +264,7 @@ $green: #c0d84a;
                   display: inline-block;
                   position: absolute;
                   right: 15%;
+                  margin-top: 2%;
                   .meta-text{
                     text-align: center;
                     margin: auto;
@@ -399,6 +400,65 @@ $green: #c0d84a;
         }
         .ver-ms-lg{
           margin-left: 56% !important;
+        }
+        .meta{
+            i {
+              left: -3% !important;
+            }
+            .meta-text{
+              margin-right: 50% !important;
+              margin-left: -11% !important;
+            }
+            .meta-value{
+              top: -30px !important;
+              left: -5px !important;
+            }
+          }
+        .btn-next{
+                width: 102% !important;
+                height: 67px !important;
+                border-radius: 0px !important;
+                margin-left: -50% !important;
+                margin-bottom: -10% !important;
+
+        }
+      }
+    }
+  }
+   @media screen and (max-width: 640px) {
+    .q-tabs-panes {
+      .q-tab-pane {
+        width: 100% !important;
+        left: 0 !important;
+        .buttons{
+          width: 110% !important;
+          left: -18px;
+          position: relative;
+          margin-bottom: -35px !important;
+          .btn-primary{
+            width: 100px !important;
+            left: -117px !important;
+          }
+          .btn-secondary{
+            background: $btn-secondary;
+          }
+          .btn-tree{
+           left: 101px !important;
+          }
+        }
+        .grafica{
+          position: relative !important;
+          // left: -12% !important;
+          margin-bottom: -15% !important;
+          .meses{
+            left: 3% !important;
+          }
+        }
+        .ver-ms-p{
+          margin-left: 59% !important;
+        }
+        .ver-ms-lg{
+          margin-left: 55% !important;
         }
         .meta{
             i {
